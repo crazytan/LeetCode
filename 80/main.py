30845d1a4868b0d78280f374e32c403c02ba0403
Jia Tan <tjtanjia.tan@gmail.com>
2015-07-15 14:26:40 +0800

    finish problem 80

---
+++ 80/main.py
@@ -0,0 +1,28 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+
+        t = False
+        p = 1
+        for i in xrange(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                if not t:
+                    t = True
+                    nums[p] = nums[i]
+                    p += 1
+            else:
+                t = False
+                nums[p] = nums[i]
+                p += 1
+
+        print nums
+        return p
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = raw_input("enter the list: ").split(',')
+    map(lambda x: int(x), nums)
+    print s.removeDuplicates(nums)
