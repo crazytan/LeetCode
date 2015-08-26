226d6b5a662166285a41d8a167bc5156c7c7fece
Jia Tan <tjtanjia.tan@gmail.com>
2015-08-26 00:07:13 -0400

    problem 104 solved

---
+++ 104/main.py
@@ -0,0 +1,30 @@
+__author__ = 'tan'
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.right), self.maxDepth(root.left))
+
+if __name__ == '__main__':
+    a = TreeNode(1)
+    b = TreeNode(2)
+    c = TreeNode(3)
+    d = TreeNode(4)
+    e = TreeNode(5)
+    c.left = d
+    c.right = e
+    a.left = b
+    a.right = c
+    print Solution().maxDepth(a)
