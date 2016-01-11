1edac01a6080729220166610128983522c1e2a8f
crazytan <tjtanjia.tan@gmail.com>
2016-01-10 22:54:17 -0500

    update 289 solution to in-place version

--- 289/Solution.java
+++ 289/Solution.java
@@ -10,28 +10,37 @@ public class Solution {
         int m = board.length;
         int n = board[0].length;
         int cnt = 0;
-        for (int i = 0;i < 8;i++) {
+        for (int i = 0;i < 4;i++) {
+            if (inside(x + dir[i][0], y + dir[i][1], m, n)) {
+                if (board[x + dir[i][0]][y + dir[i][1]] >= 2) cnt++;
+            }
+        }
+        for (int i = 4;i < 8;i++) {
             if (inside(x + dir[i][0], y + dir[i][1], m, n)) {
                 if (board[x + dir[i][0]][y + dir[i][1]] == 1) cnt++;
             }
         }
         if (now == 0) return (cnt == 3) ? 1 : 0;
-        return ((cnt >= 2) && (cnt <= 3)) ? 1 : 0;
+        return ((cnt >= 2) && (cnt <= 3)) ? 3 : 2;
     }
 
     public void gameOfLife(int[][] board) {
         if ((board == null) || (board.length == 0)) return;
         int m = board.length;
         int n = board[0].length;
-        int[][] _board = new int[m][];
+/*        int[][] _board = new int[m][];
         for (int i = 0;i < m;i++) {
             _board[i] = new int[n];
             for (int j = 0;j < n;j++)
                 _board[i][j] = board[i][j];
-        }
+        }*/
         for (int i = 0;i < m;i++)
             for (int j = 0;j < n;j++)
-                board[i][j] = next(board[i][j], _board, i, j);
+                //board[i][j] = next(board[i][j], _board, i, j);
+                board[i][j] = next(board[i][j], board, i, j);
+        for (int i = 0;i < m;i++)
+            for (int j = 0;j < n;j++)
+                board[i][j] = board[i][j] & 1;
     }
 
     public static void main(String[] args) {
