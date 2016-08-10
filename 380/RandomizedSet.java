1ef9db0924cb5ec7f2b702611ad7e5fb0195ca20
crazytan <tjtanjia.tan@gmail.com>
2016-08-10 16:15:10 -0400

    problem 380 solved

---
+++ 380/RandomizedSet.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class RandomizedSet {
+
+    private HashMap<Integer, Integer> map;
+
+    private ArrayList<Integer> list;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        this.map = new HashMap<>();
+        this.list = new ArrayList<>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        int pos = map.get(val);
+        if (pos < (list.size() - 1)) {
+            int last = list.get(list.size() - 1);
+            list.set(pos, last);
+            map.put(last, pos);
+        }
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(new Random().nextInt(list.size()));
+    }
+
+    public static void main(String[] args) {
+        RandomizedSet set = new RandomizedSet();
+        System.out.println(set.insert(1));
+        System.out.println(set.remove(2));
+        System.out.println(set.insert(2));
+        System.out.println(set.getRandom());
+        System.out.println(set.remove(1));
+        System.out.println(set.insert(2));
+        System.out.println(set.getRandom());
+    }
+}
